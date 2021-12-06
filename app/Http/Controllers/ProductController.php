ca154c19e73974018069cd1566b4ec2b2cbaec4f
azkafadhli <azkafadhli@atech.software>
2021-12-07 00:44:15 +0700

    create product with categories

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,13 +17,22 @@ class ProductController extends Controller {
         if (!User::isAdmin($user)) {
             return response()->json(['status' => 'unauthorized'], 401);
         }
+        
         $validatedData = $request->validate(
             [
-                'name' => ['max:256'],
-                'price' => ['numeric', 'gte:0']
+                'name' => ['required', 'max:256'],
+                'price' => ['required', 'numeric', 'gte:0'],
+                'category' => ['exists:categories,id']
             ]
         );
+
         $product = Product::create($validatedData);
+        
+        if (count($validatedData['category']) > 0) {
+            $category = Category::find($validatedData['category']);
+            $product->categories()->attach($category);
+        }
+        
         $product->save();
         return response()->json($product, 201);
     }
